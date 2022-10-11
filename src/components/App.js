1841cfeaf586deaa2db48478ecf9a4111ca8a49c
Karanveer Manohar <karanveermanohar30@gmail.com>
2022-10-11 17:33:36 +0000

    Update 5 files

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import NavBar from "./NavBar";
+import Filter from "./Filter";
 const App = () => {
-
   return (
     <div id="main">
-      
+      <NavBar />
+      <Filter />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
