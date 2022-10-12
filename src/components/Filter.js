7025c31d8d8428142dc3e94319ede69bad33c672
Karanveer Manohar <karanveermanohar30@gmail.com>
2022-10-12 17:42:05 +0000

    Update 7 files

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,27 +1,31 @@
 import React from "react";
-import "../styles/App.css";
-
-const Filter = () => {
+function Filter() {
   return (
-    <div className="filter-holder">
+    <div className="filter-holder" style={{ width: "100%" }}>
       <section>
         <h5>Gender</h5>
         <div>
-          <lable for="male">Male </lable>
-          <input type="radio" value="male" name="gender" id="male" />
+          <input type="radio" value="male" name="gender" id="male"></input>
+          <label for="male">Male</label>
         </div>
         <div>
-          <lable for="female">Female </lable>
-          <input type="radio" value="female" name="gender" id="female" />
+          <input type="radio" value="female" name="gender" id="female"></input>
+          <label for="female">Female</label>
         </div>
       </section>
-      
       <section>
-          <h5> Color</h5>
-          <input type="checkbox" />
+        <h5>color</h5>
+        <input type="checkbox" id="color" name="color" value="white" />
+        <label for="color"> White</label>
+      </section>
+      <section style={{ width: "100%", whiteSpace: "nowrap" }}>
+        <h5> Categories</h5>
+        <input type="checkbox" id="folded" name="folded" />
+        <label for="folded" style={{ width: "100%" }}>
+          Folded sleeve Shirts
+        </label>
       </section>
     </div>
   );
-};
-
+}
 export default Filter;
